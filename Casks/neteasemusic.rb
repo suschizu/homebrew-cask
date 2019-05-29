7893f9b5d10bbdf340b0f61e8d118f4c5ffbfacb
suschizu <brigitte@corecode.io>
2019-05-29 16:10:33 +0200

    Update neteasemusic.rb

--- Casks/neteasemusic.rb
+++ Casks/neteasemusic.rb
@@ -1,9 +1,9 @@
 cask 'neteasemusic' do
-  version '2.0.0_730'
+  version '2.1.0_772,f8f4:201942919165'
   sha256 '58163a4cf44759596dfad02ad00b1c237945dc1eb23ae1ed4cfd5868b8ba1d1d'
 
   # d1.music.126.net was verified as official when first introduced to the cask
-  url "https://d1.music.126.net/dmusic/NeteaseMusic_#{version}_web.dmg"
+  url "http://d1.music.126.net/dmusic/#{version.after_comma.before_colon}/#{version.after_colon}/NeteaseMusic_#{version.before_comma}_web.dmg"
   appcast 'https://music.163.com/api/mac/appcast.xml'
   name 'NetEase cloud music'
   name '网易云音乐'
