7e685022bce8dd623487e72659292ca574b3051c
suschizu <brigitte@corecode.io>
2019-09-12 18:40:46 +0200

    Update qmapshack.rb

--- Casks/qmapshack.rb
+++ Casks/qmapshack.rb
@@ -1,8 +1,14 @@
 cask 'qmapshack' do
-  version '1.12.0_2'
-  sha256 '560a601b17a6642617b3b3239a8e01d1cd4ba5decf998c552d56dd622446a769'
+  version '1.13.1'
 
-  url "https://bitbucket.org/maproom/qmapshack/downloads/QMapShack-MacOSX_#{version}.tar.gz"
+  if MacOS.version <= :high_sierra
+    sha256 '5c935fbb667bbb2037c8e36f4f039740234e76c55c9a10f0c750667708f12a6f'
+    url "https://bitbucket.org/maproom/qmapshack/downloads/QMapShack_OSX.10.13_#{version}.zip"
+  else
+    sha256 '500f41c1c3bbb6a99d517c75d38ddb4993405c67c3c7f77b1a94716886950463'
+    url "https://bitbucket.org/maproom/qmapshack/downloads/QMapShack_OSX.10.14_#{version}.zip"
+  end
+
   appcast 'https://bitbucket.org/maproom/qmapshack/downloads/'
   name 'QMapShack'
   homepage 'https://bitbucket.org/maproom/qmapshack/wiki/Home'
