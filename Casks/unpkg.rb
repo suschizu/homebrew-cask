c01d07a7444fb49b015a0e51d9d150dc6e8fbc94
suschizu <brigitte@corecode.io>
2019-10-27 20:29:50 +0100

    Update unpkg.rb

--- Casks/unpkg.rb
+++ Casks/unpkg.rb
@@ -1,6 +1,11 @@
 cask 'unpkg' do
-  version '4.5'
-  sha256 'e32754b07073b383320d2bc0ce4ba9bd83a3f352043bd6f500741901f00c8c17'
+  if MacOS.version <= :mojave
+    version '4.5'
+    sha256 'e32754b07073b383320d2bc0ce4ba9bd83a3f352043bd6f500741901f00c8c17'
+  else
+    version '4.6'
+    sha256 '6949b648d5cd02138f9e7f505f1fb3541905d6016ed951daa71cb7250c3ace2e'
+  end
 
   # github.com/downloads/timdoug/unpkg was verified as official when first introduced to the cask
   url "https://github.com/downloads/timdoug/unpkg/unpkg-#{version}.zip"
