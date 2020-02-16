383efdafe08ad70fb7ee920b208fdda85b014c8c
suschizu <brigitte@corecode.io>
2020-02-16 17:26:21 +0100

    Update and rename balsamiq-mockups.rb to balsamiq-wireframes.rb

--- Casks/balsamiq-wireframes.rb
+++ Casks/balsamiq-wireframes.rb
@@ -1,13 +1,13 @@
-cask 'balsamiq-mockups' do
+cask 'balsamiq-wireframes' do
   version '3.5.17'
   sha256 'a89f2a7959bc7f987710b7f3d6ae665e5765180dd02763d1b5f953415c130df2'
 
   url "https://builds.balsamiq.com/mockups-desktop/Balsamiq_Mockups_#{version}.dmg"
   appcast 'https://builds.balsamiq.com/mockups-desktop/version.jsonp'
-  name 'Balsamiq Mockups'
+  name 'Balsamiq Wireframes'
   homepage 'https://balsamiq.com/'
 
-  app "Balsamiq Mockups #{version.major}.app"
+  app "Balsamiq Wireframes.app"
 
   zap trash: [
                "~/Library/Caches/BalsamiqMockups#{version.major}.*",
