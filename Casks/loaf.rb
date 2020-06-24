256b01d89e87a418b85cfbc65a37c5b93f2eeadb
suschizu <brigitte@corecode.io>
2020-06-24 18:33:33 +0200

    Update and rename motion.rb to loaf.rb

--- Casks/loaf.rb
+++ Casks/loaf.rb
@@ -1,14 +1,14 @@
-cask 'motion' do
-  version '0.5.0'
+cask 'loaf' do
+  version '1.0.0'
   sha256 '9a9c57dcebd6c172eef1035c3aefdab35f1845ab02c0ffdcceb256cf567dcf80'
 
-  # github.com/philipardeljan/getmotion/ was verified as official when first introduced to the cask
-  url "https://github.com/philipardeljan/getmotion/releases/download/v#{version}/motion.dmg"
+  # github.com/philipardeljan/getloaf/ was verified as official when first introduced to the cask
+  url "https://github.com/philipardeljan/getloaf/releases/download/v#{version}/loaf.dmg"
   appcast 'https://github.com/philipardeljan/getmotion/releases.atom'
-  name 'Motion'
+  name 'Loaf'
   homepage 'https://www.getmotion.io/'
 
-  app 'Motion.app'
+  app 'Loaf.app'
 
   zap trash: [
                '~/Library/Application Support/Motion (beta)',
