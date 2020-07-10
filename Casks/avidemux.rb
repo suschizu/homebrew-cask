0670e1190427c4b9f2074d156e330311ffd5446b
suschizu <brigitte@corecode.io>
2020-07-10 19:17:26 +0200

    Update avidemux.rb

--- Casks/avidemux.rb
+++ Casks/avidemux.rb
@@ -1,9 +1,9 @@
 cask 'avidemux' do
-  version '2.7.4'
-  sha256 'a5c5028ecc954b6658b4c0e6b04c1c186c42a12530e66a5379f51fe7a3ebfcd8'
+  version '2.7.6'
+  sha256 '4641d0d8f79dceddfcec6fe24d94dd6dd76e963e552711964afdd3fc92b15e46'
 
-  # sourceforge.net/avidemux/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/avidemux/avidemux/#{version}/Avidemux_#{version}_Mojava_64Bits_Qt5.dmg"
+  # fosshub.com/Avidemux.html/ was verified as official when first introduced to the cask
+  url "http://www.fosshub.com/Avidemux.html/Avidemux_#{version}_Catalina_64Bits_Qt5.dmg"
   appcast 'https://sourceforge.net/projects/avidemux/rss?path=/avidemux'
   name 'Avidemux'
   homepage 'https://www.avidemux.org/'
